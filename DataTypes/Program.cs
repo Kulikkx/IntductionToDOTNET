1e27f16d943ec21d1f7be91fac217c7d036640eb
Kulik_kx07@top-academy.ru <Kulik_kx07@top-academy.ru>
2024-09-09 18:58:56 +0300

    Add project files.

---
+++ DataTypes/Program.cs
@@ -0,0 +1,82 @@
+﻿//#define NUMERIC_TYPES
+//#define LITERALS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static readonly string delimiter1 = "\n__________________________________\n";
+        static readonly string delimiter2 = "\n----------------------------------\n";
+        static void Main(string[] args)
+        {
+#if NUMERIC_TYPES
+//...
+            Console.WriteLine("C# DataTypes");
+
+
+            #region Boolean
+            //Console.WriteLine(bool.TrueString);
+            //Console.WriteLine(Convert.ToBoolean(1));
+            //Console.WriteLine(bool.Parse(0)); 
+            #endregion
+
+            #region Char
+            ////Unicode
+            //Console.WriteLine(char.GetNumericValue(char.MinValue));
+            //Console.WriteLine(Char.MaxValue); 
+            #endregion
+
+
+            #region int
+            //Console.Write($"Перепенная типа 'char' занимает {sizeof(short)} байта, "});
+            //Console.WriteLine($ "и принимает значения в диапазоне: {short.MinValue}...{short.MaxValue}.");
+            //Console.Write($"Диапазон принимаемых значений 'ushort': {ushort.MaxValue};" );
+            //Console.WriteLine(delimiter1);
+            //Console.Write($ "Перепенная типа 'char' занимает {sizeof(int16)} байта памяти, "});
+            //Console.WriteLine($ "и принимает значения в диапазоне: {int16.MinValue}...{int16.MaxValue}.");
+            //Console.Write($ "Диапазон принимаемых значений 'Uint16': {uint16.MaxValue};" ); 
+            #endregion
+
+            #region IntegralTypes
+            //Console.Write($"Переменная типа 'int' занимает {sizeof(int)} байта памяти,");
+            //Console.WriteLine($"и принимает значения в диаазоне: {int.MinValue}....{int.MaxValue},");
+            //Console.WriteLine($"Диапазон принимаемых значений 'UInt16' "); 
+            #endregion
+
+            double a = 12.56;
+            Console.WriteLine(a * 100000);
+#endif
+#if LITERALS
+//...
+
+            Console.WriteLine(123.GetType()); 
+#endif
+
+            #region Conversions1
+            //Console.WriteLine(((byte)5).GetType().Name); 
+            #endregion
+            try
+            {
+
+            int a = int.MaxValue;
+            uint b = uint.MaxValue;
+            Console.WriteLine(a + b);
+            Console.WriteLine((a + b).GetType().Name);
+            long c = long.MaxValue;
+            Console.WriteLine(b + c);
+            Console.WriteLine((b + c).GetType());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Buy");
+        }
+    }
+}
---
+++ IntroductionToDOTNET/Program.cs
@@ -0,0 +1,69 @@
+﻿#define OUTPUT_TO_SCREEN
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToDOTNET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if OUTPUT_TO_SCREEN
+            Console.Title = "Introduction to.NET";
+            Console.WriteLine("\t\tHello.NET");
+            //Console.BackgroundColor= ConsoleColor.Green;//цвет фона
+            //Console.ForegroundColor= ConsoleColor.Blue;//цвет текста
+
+            Console.SetCursorPosition(16, 5);
+            Console.CursorLeft = 24; //координата курсора по оси х
+            Console.CursorTop = 5; //координата курсора по оси у
+            Console.WriteLine("Вот так задаются координаты");
+            Console.ResetColor(); //применяет цветовую схему по умолчанию
+
+            //ЗАДАЕМ РАЗМЕРЫ ОКНА
+            //Console.WindowWidth = 44;
+            //Console.WindowHeight=16;
+            Console.SetWindowSize(64, 16);
+
+            // Для того чтобы свернуть часть кода, этот код нужно поместить в region
+            #region CodeSnippet
+            //ЗАДАЕМ ПОЛОЖЕНИЕ ОКНА НА ЭКРАНЕ
+            //Console.WindowLeft = 10;
+            //Console.WindowTop=10;
+            #endregion
+
+            //Console.SetWindowPosition(10, 10);
+            //Задаем размер буфера консоли
+            Console.BufferWidth = Console.WindowWidth;
+            Console.BufferHeight = Console.WindowHeight;
+#endif
+
+            Console.Write("Введите Ваше имя: ");
+            string first_name=Console.ReadLine(); // Метод ReadLine читает строку с клавитатуры до нажатия Enter и возвращает прочитанную строку
+            Console.WriteLine(first_name);
+
+            Console.Write(" Введите Вашу фамилию: ");
+            String last_name = Console.ReadLine();
+            Console.WriteLine(last_name);
+
+            Console.Write("Введите Ваш возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            //Класс convert представляет соой набор статических методов для преобразования типов. 
+            // Этот класс используется тогда, когда другие преобразования не работают.
+
+            //Console.WriteLine(a);
+
+            //Операции над строками
+            Console.WriteLine(last_name + " " + first_name + " " + age + " years"); //Конкатенации строк
+            // Кроме конкатенации строк, С# поддерживает форматирование строк.
+
+            Console.WriteLine(String.Format("{0} {1} {2} years", last_name, first_name, age));
+
+            Console.WriteLine($"{last_name} {first_name} {age} years"); // Интерполяция - это изменение искажения
+
+        }
+    }
+}
