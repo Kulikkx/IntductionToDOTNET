ffe088c0a1faa2707e23fe4acbd95eec8fa3bee8
Kulik_kx07@top-academy.ru <Kulik_kx07@top-academy.ru>
2024-09-23 00:17:11 +0300

    DONE

--- Fraction/Program.cs
+++ Fraction/Program.cs
@@ -1,37 +1,66 @@
-﻿using System;
+﻿//#define CONSTRUCTORS_CHECK
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Fraction
 {
-
-class Fraction
+    class Program
     {
-        public int Integer { get; set; } //Автосвойство, которые не требует объявления переменной, посколькому сами ее объявляют
-    public int Numerator {  get; set; }
-        int denominator;
-        //обычне свойства, которые применяются к переменной denominator
-        public int Denominator
-        { 
-            get=>denominator; 
-            set=>denominator=value==0 ? 1: value; 
-        }
-        // Constructor
-
-        // Methods:
-        public void Print()
+        static void Main(string[] args)
         {
-            if (Integer != 0) Console.Write(Integer);
-            if (Numerator != 0)
-            {
-                Console.Write("(");
-                Console.Write( $"{Numerator}/{Denominator}");
-                if (Integer != 0) Console.Write(")");
+#if CONSTRUCTORS_CHECK
+            Fraction A = new Fraction();
+            A.Print();
+            Console.WriteLine(A);
 
+            Fraction B = new Fraction(5);
+            B.Print();
+            Console.WriteLine(B);
+
+            Fraction C = new Fraction(1, 2);
+            C.Print();
+            Console.WriteLine(C);
+
+            Fraction D = new Fraction(2, 3, 4);
+            D.Print();
+            Console.WriteLine(D.ToString());
+            Console.WriteLine(D.GetType()); 
+#endif
+            Fraction A = new Fraction(2, 3, 4);
+            Fraction B = new Fraction(3, 4, 5);
+            Fraction C = new Fraction(A * B);
+            Fraction D = new Fraction(A / B);
+            Fraction E = new Fraction(A + B);
+            Fraction F = new Fraction(A - B);
+            Fraction G = new Fraction(--A);
+            Fraction H = new Fraction(++A);
+            Fraction I = new Fraction(A += B);
+            Fraction J = new Fraction(A -= B);
+
+            A.Print();
+            B.Print();
+            C.Print();
+            Console.WriteLine(C.Inverted());
+            D.Print();
+            E.Print();
+            F.Print();
+            G.Print();
+            H.Print();
+            I.Print();
+            J.Print();
+            A *= B;
+            A.Print();
+            A /= B;
+            A.Print();
+            for (Fraction i = new Fraction(1, 2); i < new Fraction(10); i++)
+            {
+                Console.Write(i + "\t");
             }
-            else if(Integer==0)
+            Console.WriteLine();
         }
     }
- }
+}
